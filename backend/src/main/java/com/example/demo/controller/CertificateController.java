da644c4b0c57815752e766fd4c7d2cc33acdb254
nevenaatic <atic.ra67.2018@uns.ac.rs>
2022-04-11 00:08:12 +0200

    feat: method for validation

--- backend/src/main/java/com/example/demo/controller/CertificateController.java
+++ backend/src/main/java/com/example/demo/controller/CertificateController.java
@@ -3,16 +3,27 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.CertificateService;
 
 @Controller
 //@CrossOrigin
 @RequestMapping(value="certificate")
 public class CertificateController {
+	
+	private CertificateService certificateService;
+	
+	public CertificateController(CertificateService certificateService ) {
+		this.certificateService = certificateService;
+	}
 
 	@PostMapping(path = "/createCertificate")
 	public boolean createCertificate()
@@ -45,4 +56,14 @@ public class CertificateController {
 	{
 		return null;
 	}
+	
+	@PostMapping(value = "/checkValidity/{serialCode}")
+	public ResponseEntity<Boolean> isCertificateValid(@PathVariable String serialCode) {
+		
+	
+		return new ResponseEntity<> (certificateService.isCertificateValid(serialCode), HttpStatus.ACCEPTED);
+		  
+	}
+	
+	
 }
